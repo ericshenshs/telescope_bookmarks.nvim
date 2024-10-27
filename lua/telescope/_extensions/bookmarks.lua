9bf298edb3bd4ca6beed969e807ae6387fa490cd
ericshenshs@metamac <eric.shen.shs@gmail.com>
2024-10-27 00:14:41 -0400

    test

--- lua/telescope/_extensions/bookmarks.lua
+++ lua/telescope/_extensions/bookmarks.lua
@@ -2,14 +2,36 @@ local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local sorters = require("telescope.sorters")
 local previewers = require("telescope.previewers")
+local action_state = require("telescope.actions.state")
 local find_files = require('telescope.builtin').find_files
 
+local function delete_file(prompt_bufnr)
+  local entry = action_state.get_selected_entry()
+  local file_path = entry.path or entry.filename
+  local confirm = vim.fn.input("Delete " .. file_path .. "? (y/n): ")
+  if confirm:lower() == "y" then
+    vim.fn.delete(file_path)
+    actions.close(prompt_bufnr)
+    print("Deleted " .. file_path)
+  else
+    print("Canceled deletion")
+  end
+end
+
 local function bookmarks_picker(opts)
+  -- Inherits the find_files picker.
+  -- with parameter overrides.
   opts = vim.tbl_deep_extend("force", {
     prompt_title = "Bookmarks",
+    attach_mappings = function(prompt_bufnr, map)
+      -- Custom keymap: Open URL in browser
+      map("n", "d", delete_file)
+      map("n", "p", delete_file)
+      return true
+    end
   }, opts or {})
 
-  -- Define a new picker bookmarks
+  -- Inherits the find_files picker.
   return find_files(opts)
 end
 
